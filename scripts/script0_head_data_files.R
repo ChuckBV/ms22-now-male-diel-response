a954448ec28131741ef32583765dade9162c4da2
Chuck Burks <r.dominica@gmail.com>
2021-05-13 19:53:37 -0700

    Determining number of observations

--- scripts/script0_head_data_files.R
+++ scripts/script0_head_data_files.R
@@ -86,5 +86,80 @@ x <- x[complete.cases(x), ] # retains 95% of obs
 x$site <- factor(x$site, levels = unique(x$site))
    # Looks like x has to be a factor for this to work
 y <- split(x, f = x$site)
+y[[1]][1:6,] 
+y[[2]][1:6,] 
+y[[3]][1:6,] 
+y[[4]][1:6,] 
+y[[5]][1:6,] 
+   # split made this into a list containting 5 data frames. Notation above
+   # is a klunky way of heading each
 
-# https://www.talkstats.com/threads/differences-between-elements-of-a-vector.13341/
+### Subset back out entire data sets
+UCKearney  <- y[[1]] 
+MWoolf_east  <- y[[2]] 
+MWoolf_west  <- y[[3]] 
+Perez  <- y[[4]] 
+usda <- y[[5]]
+
+### Pull all times out of y[[1]] (UCKearney) and place in a vector to examine
+### getting time differences
+dtimes1 <- y[[1]][,1]  # 8244 values
+dtimes1 <- dtimes1[!is.na(dtimes1)] 
+length(dtimes1) # 8244, no loss
+z <- dtimes1[1:10] # first 10 values
+
+### diff functions
+### https://www.talkstats.com/threads/differences-between-elements-of-a-vector.13341/
+### https://stackoverflow.com/questions/30510044/how-to-make-time-difference-in-same-units-when-subtracting-posixct
+
+a <- seq(1,10) # diff() with an integer variable
+diff(a)
+# [1] 1 1 1 1 1 1 1 1 1
+
+diff(z) # diff function with times from y$UCKearney
+# Time differences in mins
+# [1] 60 61 59 61 60 61 60 60 60
+
+### Use diff with the entire string and units()<- to control unit
+b <- diff(dtimes1)
+str(b)
+units(b) <- "mins"
+length(b) #8243 obs, 1 less than dtimes
+unique(b)
+
+difs1 <- as.integer(diff(y$UCKearney[,1])) # y$UCKearney is 8254 rows
+unique(difs1)
+
+### Try using diff within the dataframe
+MWoolf_west$dif_time[2:nrow(MWoolf_west)] <- diff(MWoolf_west$datetime)
+MWoolf_west$dif_time <- MWoolf_west$dif_time/60 
+    # manually convert from seconds to minutes
+
+### What time intervals are frequent?
+MWoolf_west %>% 
+  group_by(dif_time) %>% 
+  summarise(nObs = n()) %>% 
+  filter(nObs > 10)
+# A tibble: 9 x 2
+# dif_time  nObs
+#     <dbl> <int>
+# 1   0.0167    49
+# 2  25        118
+# 3  29         30
+# 4  30.0       41
+# 5  30       6261
+# 6  31        749
+# 7  55         36
+# 8  60        131
+# 9  61         32
+    # Various time intervals, hard to say why without comments column
+
+### How many observations per day?
+MWoolf_west$caldat <- as.Date(MWoolf_west$datetime)
+
+Mwwest2 <- MWoolf_west %>% 
+  group_by(caldat) %>% 
+  summarise(nObs = n())
+   # Before 2019-05-30 one image per hour; after 2
+
+
--- scripts/script0b_clock_package.R
+++ scripts/script0b_clock_package.R
@@ -21,3 +21,40 @@ date_build(2019, 2, 1:5)
 #  - date_floor(), date_ceiling(), and date_round()
 #  - date_shift()
 
+### https://clock.r-lib.org/articles/recipes.html#generate-sequences-of-dates-and-date-times-1
+### Use copy and past from above vignette to figure out this new toy
+from <- as_naive_time(year_month_day(2019, 1, 1, 2, 30, 00))
+from
+# <time_point<naive><second>[1]>
+# [1] "2019-01-01 02:30:00"
+
+to <- as_naive_time(year_month_day(2019, 1, 1, 12, 30, 00))
+to
+# <time_point<naive><second>[1]>
+# [1] "2019-01-01 12:30:00"
+
+### Get a vector of datetime values at 30 minut intervals
+x <- seq(from, to, by = duration_minutes(30))
+# <time_point<naive><second>[21]>
+#   [1] "2019-01-01 02:30:00" "2019-01-01 03:00:00" "2019-01-01 03:30:00"
+# [4] "2019-01-01 04:00:00" "2019-01-01 04:30:00" "2019-01-01 05:00:00"
+# [7] "2019-01-01 05:30:00" "2019-01-01 06:00:00" "2019-01-01 06:30:00"
+# [10] "2019-01-01 07:00:00" "2019-01-01 07:30:00" "2019-01-01 08:00:00"
+# [13] "2019-01-01 08:30:00" "2019-01-01 09:00:00" "2019-01-01 09:30:00"
+# [16] "2019-01-01 10:00:00" "2019-01-01 10:30:00" "2019-01-01 11:00:00"
+# [19] "2019-01-01 11:30:00" "2019-01-01 12:00:00" "2019-01-01 12:30:00"
+
+### Make into POSIXct (more generic R)
+x <- as.POSIXct(x)
+str(x)
+# POSIXct[1:21], format: "2019-01-01 02:30:00" "2019-01-01 03:00:00" "2019-01-01 03:30:00" ...
+
+### Try diff function with current vector x
+y <- diff(x)
+y
+# Time differences in mins
+# [1] 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30
+
+length(y)
+# [1] 20
+    # 1 less than x, as should be
