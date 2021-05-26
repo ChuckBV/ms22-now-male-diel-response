e6f81ca6b26ae00b431130f01a65e87b4d72f232
Chuck Burks <r.dominica@gmail.com>
2021-05-25 17:25:19 -0700

    Simple graphic way to show intervals

--- scripts/script0_head_data_files.R
+++ scripts/script0_head_data_files.R
@@ -106,142 +106,27 @@ length(allsites19$event[!is.na(allsites19$event)])
 unique(allsites19$event) 
 # [1] NA                       "Sticky roll tweak"      "Replace of sticky roll"
 
-# drop event which is 95% NA and not essential. Use Base R to drop, then select
-# only complete records
-
 allsites19 <- allsites19[ ,-5] 
 allsites19 <- allsites19[complete.cases(allsites19), ] 
+allsites19
 
 # retains 95% of obs. Now making site into a factor to use it with the split function
 # Looks like x has to be a factor for this to work
 
-allsites19$site <- factor(allsites19$site, levels = unique(allsites19$site))
-
-allsites19_L <- split(allsites19, f = allsites19$site)
-allsites19_L[[1]][1:6,] 
-allsites19_L[[2]][1:6,] 
-allsites19_L[[3]][1:6,] 
-allsites19_L[[4]][1:6,] 
-allsites19_L[[5]][1:6,] 
-
-# split made this into a list containing 5 data frames. Notation above
-# is a klunky way of heading each. Subset back out entire data sets
-
-UCKearney  <- allsites19_L[[1]]
-MWoolf_east  <- allsites19_L[[2]]
-MWoolf_west  <- allsites19_L[[3]]
-Perez  <- allsites19_L[[4]]
-usda <- allsites19_L[[5]]
-
-# Pull all times out of UCKearney and place in a vector to examine getting time 
-# differences. First verify length and show the first 10 values of this vector.
-# Note that times are Pacific Daylight, but R pressumes universal time (UTC)
-
-dtimes1 <- UCKearney$datetime  # 8244 values
-length(dtimes1[!is.na(dtimes1)]) 
-dtimes1[1:10]
-# [1] "2019-04-26 13:56:00 UTC" "2019-04-26 14:56:00 UTC" "2019-04-26 15:57:00 UTC" "2019-04-26 16:56:00 UTC"
-# [5] "2019-04-26 17:57:00 UTC" "2019-04-26 18:57:00 UTC" "2019-04-26 19:58:00 UTC" "2019-04-26 20:58:00 UTC"
-# [9] "2019-04-26 21:58:00 UTC" "2019-04-26 22:58:00 UTC"
-
-# One approach it to get the interval between the times in "dtimes1".
-# Use diff() with units()<- to control unit
-
-b <- diff(dtimes1)
-units(b) <- "mins"
-length(b) 
-
-# 8243 obs, 1 less than dtimes. Show first 10 values
-
-b[1:10]
-# Time differences in mins
-# [1] 60 61 59 61 60 61 60 60 60 60
-
-# Show unique values
-
-unique(b)
-
-
-### Try using diff within the dataframe
-MWoolf_west$dif_time[2:nrow(MWoolf_west)] <- diff(MWoolf_west$datetime)
-MWoolf_west$dif_time <- MWoolf_west$dif_time/60 
-    # manually convert from seconds to minutes
-
-### What time intervals are frequent?
-MWoolf_west %>% 
-  group_by(dif_time) %>% 
-  summarise(nObs = n()) %>% 
-  filter(nObs > 10)
-# A tibble: 9 x 2
-# dif_time  nObs
-#     <dbl> <int>
-# 1   0.0167    49
-# 2  25        118
-# 3  29         30
-# 4  30.0       41
-# 5  30       6261
-# 6  31        749
-# 7  55         36
-# 8  60        131
-# 9  61         32
-    # Various time intervals, hard to say why without comments column
-
-## How many observations per day? This is simpler and cruder approach
-
-### Starting with 2019 data
-
-MWoolf_west$caldat <- as.Date(MWoolf_west$datetime)
-
-Mwwest2 <- MWoolf_west %>% 
-  group_by(caldat) %>% 
-  summarise(nObs = n())
-   # Before 2019-05-30 one image per hour; after 2
-Mwwest2
-Mwwest2[Mwwest2$nObs > 25, ]
-  # First date with >25 is 5/31
-
-
-MWoolf_east$caldat <- as.Date(MWoolf_east$datetime)
-
-Mweast2 <- MWoolf_east %>% 
-  group_by(caldat) %>% 
+x19 <- allsites19 %>% 
+  mutate(caldate = as.Date(datetime)) %>% # group into days
+  group_by(site, caldate) %>% 
   summarise(nObs = n())
 
-Mweast2
-Mweast2[Mweast2$nObs > 25, ]
-  # First date with >25 is 6/14
+# Plot readings per day by site
 
-Perez$caldat <- as.Date(Perez$datetime)
+ggplot(x19, aes(x = caldate, y = nObs)) +
+  geom_line() + 
+  facet_grid(site ~ .)
 
-Perez2 <- Perez %>% 
-  group_by(caldat) %>% 
-  summarise(nObs = n())
+# When needed filter x for nObs > 42
 
-Perez2
-Perez2[Perez2$nObs > 25, ]
-  # First date with >25 is 6/04. More consistent after 6/14
-
-UCKearney$caldat <- as.Date(UCKearney$datetime)
-
-UCKearney2 <- UCKearney %>% 
-  group_by(caldat) %>% 
-  summarise(nObs = n())
-
-UCKearney2
-UCKearney2[UCKearney2$nObs > 25, ]
-   # Consistently ~48/day from 5/31 on
-
-usda$caldat <- as.Date(usda$datetime)
-
-usda2 <- usda %>% 
-  group_by(caldat) %>% 
-  summarise(nObs = n())
-
-usda2
-usda2[usda2$nObs > 25, ]
-   # Consistently ~48/day from 4/27 on
-
-### Continuing with 2020 dat
+### Continuing with 2020 data
 
 allsites20 <- allsites20[ ,-5] 
 allsites20 <- allsites20[complete.cases(allsites20), ]
@@ -249,18 +134,18 @@ allsites20
 
 # Create temporary data frame containing readings per day
 
-x <- allsites20 %>% 
+x20 <- allsites20 %>% 
   mutate(caldate = as.Date(datetime)) %>% # group into days
   group_by(site, caldate) %>% 
   summarise(nObs = n())
 
 # Plot readings per day by site
 
-ggplot(x, aes(x = caldate, y = nObs)) +
+ggplot(x20, aes(x = caldate, y = nObs)) +
   geom_line() + 
   facet_grid(site ~ .)
 
-# When needed filter x for 
+# When needed filter x for nObs > 42
 
 
 
