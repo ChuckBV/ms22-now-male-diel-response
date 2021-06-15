556706fe6d7d8934e993b45a457fb732dbebc15c
Chuck Burks <r.dominica@gmail.com>
2021-06-15 16:33:23 -0700

    Cleaning scripts no longer used

--- scripts/script2_nightly_variation_moth_capture.R
+++ scripts/script2_nightly_variation_moth_capture.R
@@ -2,7 +2,7 @@
 # script2_nightly_variation_moth_capture
 # 
 # Examine variation in nightly temperatures, and determine the number of
-# observations in a range relevant to shiftying mating activity earler
+# observations in a range relevant to shifting mating activity earlier
 #
 #===========================================================================#
 
@@ -77,5 +77,58 @@ unique(CountsGt20$site)
 # four of the five sites represented, but more the two Mike Woolf sites than 
 # the others. Only 34 nights total. CountsGt20 provides a way to filter 2019
 # data by Julian date
+write.csv(CountsGt20,
+          "./nights_w_counts_gt20_y19.csv",
+          row.names = FALSE)
 
+# Similar characterization for 2020
+nightly20 <- allsites20 %>% 
+  mutate(Julian = yday(datetime)) %>% 
+  group_by(site, Julian) %>% 
+  summarise(nObs = n(),
+            NowPrDay = sum(pest_dif, na.rm = TRUE))
+nightly20
+# A tibble: 729 x 4
+# Groups:   site [5]
+#   site       Julian  nObs NowPrDay
+#   <chr>       <dbl> <int>    <dbl>
+# 1 mikewoolf1    113    27        8
+# 2 mikewoolf1    114    38        0
+# 3 mikewoolf1    115    32        4
 
+# Season summary of days and captures by site
+nightly20 %>% 
+  group_by(site) %>% 
+  summarise(Min = min(Julian),
+            Max = max(Julian),
+            nDays = n_distinct(Julian),
+            NowPrDay = sum(NowPrDay, na.rm = TRUE))
+# A tibble: 5 x 5
+#   site         Min   Max nDays NowPrDay
+#   <chr>      <dbl> <dbl> <int>    <dbl>
+# 1 mikewoolf1   113   266   146      294
+# 2 mikewoolf2    72   266   179      394
+# 3 mikewoolf3   112   266   145      257
+# 4 mikewoolf4    66   266   181      376
+# 5 mikewoolf5   189   266    78       64
+
+CountsGt20_y20 <- nightly20 %>% 
+  filter(NowPrDay >= 20)
+FSA::headtail(CountsGt20_y20)
+#    site Julian nObs NowPrDay
+# 1  mikewoolf1    170   49       20
+# 2  mikewoolf1    174   22       25
+# 3  mikewoolf1    265   50       21
+# 8  mikewoolf4    261   48       20
+# 9  mikewoolf4    263   47       20
+# 10 mikewoolf4    265   51       38
+
+unique(CountsGt20_y20$site)
+# [1] "mikewoolf1" "mikewoolf2" "mikewoolf3" "mikewoolf4"
+
+# four of the five sites represented, but more the two Mike Woolf sites than 
+# the others. Only 34 nights total. CountsGt20 provides a way to filter 2019
+# data by Julian date
+write.csv(CountsGt20,
+          "./nights_w_counts_gt20_y20.csv",
+          row.names = FALSE)
