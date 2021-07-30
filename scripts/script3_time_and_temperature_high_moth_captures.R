ae3e2ec1d8c711e601b6519c6c195b1340260840
Chuck Burks <r.dominica@gmail.com>
2021-07-29 17:21:14 -0700

    Progress

--- scripts/script3_time_and_temperature_high_moth_captures.R
+++ scripts/script3_time_and_temperature_high_moth_captures.R
@@ -4,13 +4,26 @@
 # Examine temperatures and times of capture fore sites and nights with high
 # moth capture. Note that the gt20 files are are from the "scrubbed data"
 #
+# This script uses the "greater than 20" file to select traps and days 
+# examined. These observations are defined and selected in script 2, and
+# essentially means that only trap-day combinations with >20 moths trapped
+# are considered. This is merged back onto the total data set to get an
+# hourly trace. 
+#
+# PARTS
+# 1. Load previous "gt20" files into global memory (and other input files) (24)
+# 2. Merge and plot 2019 data (64)
+# 3. Merge and plot 2020 data 
+#
 #===========================================================================#
 
 library(tidyverse)
 library(lubridate)
 library(timechange)
 
-# Load previous "gt20" files into memory
+#---------------------------------------------------------------------------
+# 1. Load previous "gt20" files into global memory (and other input files)
+#---------------------------------------------------------------------------#
 gt20_y19 <- readr::read_csv("./data/nights_w_counts_gt20_y19.csv")
 gt20_y19
 # A tibble: 34 x 5
@@ -47,6 +60,11 @@ alltemps20$site[alltemps20$site == "MWT3"] <- "mikewoolf3"
 alltemps20$site[alltemps20$site == "MWT4"] <- "mikewoolf4"
 alltemps20$site[alltemps20$site == "MWT5"] <- "mikewoolf5"
 
+#---------------------------------------------------------------------------
+# 2. Merge and 2019 and 2020 data
+#---------------------------------------------------------------------------#
+
+# Modify for merge compatibility)
 alltemps19 <- alltemps19 %>% 
   dplyr::rename(datetime = Date_time)
 alltemps20 <- alltemps20 %>% 
@@ -68,7 +86,9 @@ counts20 <- dplyr::left_join(gt20_y20,allsites20)
 temps20 <- dplyr::left_join(gt20_y20,alltemps20)
   # by = c("site","Julian"), reduces from 1750 to 218 obs
 
-# Determine how to combine data sets in tidy format for ggplot
+#---------------------------------------------------------------------------
+# 3. clean and plot 2019 data
+#---------------------------------------------------------------------------#
 head(counts19,2) # 1534 obs
 # A tibble: 2 x 10
 #   site         Year Julian  nObs NowPrDay datetime            pest_nmbr pest_dif reviewed event
@@ -118,7 +138,7 @@ ggplot(counts19, aes(x = since_night2, y = pest_dif)) +
   geom_point() +
   ylim(1,50) +
   facet_grid(site ~ .)
-######
+##### #
 # Results
 # - Most obs between 3 and 6:30 (drill in to confirm)
 # - Can examine individual obs outside this time
@@ -131,9 +151,10 @@ x <- counts19 %>%
 
 ### 26 observations. The most "unusual is 7 moths at 11AM 2019-08-16, MWoolf-east
 
-# Attempt to duplicate with the 2020 data sets 
 
-
+#---------------------------------------------------------------------------
+# 2. clean and plot 2020 data
+#---------------------------------------------------------------------------#
 
 # Determine how to combine data sets in tidy format for ggplot
 head(counts20,2) 
@@ -167,7 +188,7 @@ counts20$datetime <- timechange::time_force_tz(counts20$datetime, tz = realtz)
 attr(counts20$datetime, "tzone")
 # [1] "America/Los_Angeles"
 
-# Get time since zero hour
+# Get time since zero hour (midnight)
 counts20$mnight <- lubridate::make_datetime(year(counts20$datetime),
                                                  month(counts20$datetime),
                                                  day(counts20$datetime),
@@ -189,3 +210,6 @@ ggplot(counts20, aes(x = since_night2, y = pest_dif)) +
   facet_grid(site ~ .)
 
 ### Overall patterns similar to 2019
+x2 <- counts20 %>% 
+  filter(pest_dif > 0) %>% 
+  filter(since_night2 < 2.5 | since_night2 > 6.5)
---
+++ scripts/script4_time_and_temperature_all_moth_captures.R
@@ -0,0 +1,59 @@
+#===========================================================================#
+# script4_time_and_temperature_all_moth_captures.R
+# 
+#
+# PARTS
+# 1. Load combined 2019 and 2020 count and temperature data files
+# 2. Merge and plot 2019 data (64)
+# 3. Merge and plot 2020 data 
+#
+#===========================================================================#
+
+library(tidyverse)
+library(lubridate)
+library(timechange)
+
+#---------------------------------------------------------------------------
+# 1. Load and clean 2019 and 2020 data files (same as script3)
+#---------------------------------------------------------------------------#
+
+# Load original data files into memory
+allsites19 <- read.csv("./allsites_y19_scrubbed.csv") 
+allsites20 <- read.csv("./allsites_y20_scrubbed.csv")
+alltemps19 <- readr::read_csv("./trapview_temps_degf_y19.csv")
+alltemps20 <- readr::read_csv("./trapview_temps_degf_y20.csv")
+
+# Examine and fix discrepancy in site names in 2020 capture and temperature data
+unique(allsites20$site)
+# [1] "mikewoolf1" "mikewoolf2" "mikewoolf3" "mikewoolf4" "mikewoolf5"
+unique(alltemps20$site)
+# [1] "MWT1" "MWT2" "MWT3" "MWT4" "MWT5"
+
+alltemps20$site[alltemps20$site == "MWT1"] <- "mikewoolf1"
+alltemps20$site[alltemps20$site == "MWT2"] <- "mikewoolf2"
+alltemps20$site[alltemps20$site == "MWT3"] <- "mikewoolf3"
+alltemps20$site[alltemps20$site == "MWT4"] <- "mikewoolf4"
+alltemps20$site[alltemps20$site == "MWT5"] <- "mikewoolf5"
+
+#---------------------------------------------------------------------------
+# 2. Merge and 2019 and 2020 data
+#---------------------------------------------------------------------------#
+
+# Modify for merge compatibility)
+alltemps19 <- alltemps19 %>% 
+  dplyr::rename(datetime = Date_time)
+alltemps20 <- alltemps20 %>% 
+  dplyr::rename(datetime = Date_time)
+
+alltemps19
+# A tibble: 18,969 x 6
+# site      datetime            degf_avg degf_lo degf_hi rh_avg
+#   <chr>     <dttm>                 <dbl>   <dbl>   <dbl>  <dbl>
+# 1 UCKearney 2019-05-16 17:00:00     56.1    55.4    56.8   83.0
+# 2 UCKearney 2019-05-16 18:00:00     57.5    56.1    58.6   81.7
+# 3 UCKearney 2019-05-16 19:00:00     55.0    53.8    56.8   90.4
+
+# how many temperature records per day?
+alltemps19 %>% 
+  mutate(Yr = year(datetime),
+         Julian = yday(datetime))
