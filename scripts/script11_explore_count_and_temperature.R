de4f1f2149886478ed8e5aa8d13a85c738b2943e
Chuck Burks <r.dominica@gmail.com>
2021-10-22 20:49:18 -0700

    dplyr:: to specify namespace

--- scripts/script11_explore_count_and_temperature.R
+++ scripts/script11_explore_count_and_temperature.R
@@ -251,7 +251,7 @@ combined2$Mnth.x <- droplevels(combined2$Mnth.x)
 # Plot of observations by hour with vertically aligned panels for month
 y <- combined2 %>% 
   group_by(Mnth.x,Hr) %>% 
-  summarise(counts = sum(pest_dif))
+  dplyr::summarise(counts = sum(pest_dif))
 
 sort(unique(y$Hr))
 # [1]  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
