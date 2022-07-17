23c8380f156f41d82a80e7e1ffef860b532d246c
Chuck Burks <r.dominica@gmail.com>
2022-07-17 15:31:59 -0700

    Updating repository

--- scripts/script2_clean_save_summarize_temperature_data.R
+++ scripts/script2_clean_save_summarize_temperature_data.R
@@ -1,10 +1,10 @@
 #===========================================================================#
-# script10_moth_captures_by_hour.R
+# script2_clean_save_summarize_temperature_data.R
 # 
 # PARTS
 # 1. Load combined 2019 and 2020 temperature data files (line 19)
-# 2. Merge and 2019 and 2020 count data (line 25)
-# 3. Seasonal overview using just count data (line 82)
+# 2. Add useful descriptors and save 2-year temperature data set (line 103)
+# 3. Merge all count and temperature data then save count data (line 133)
 #    - p1: Daily count by site (line graph) for 2019
 #    - p2: Daily count by site (line graph) for 2020
 #
@@ -16,50 +16,79 @@ library(scales)
 library(ggpubr)
 
 #---------------------------------------------------------------------------#
-#-- 1. Load and clean 2019 and 2020 data files (same as script3) ------------
+#-- 1. Load and clean 2019 and 2020 temperature data files       ------------
 #---------------------------------------------------------------------------#
 
-alltemps19 <- readr::read_csv("./data/trapview_temps_degf_y19.csv")
-alltemps20 <- readr::read_csv("./data/trapview_temps_degf_y20.csv")
+### Obtain and tweak 2019 temperatures
 
-### Merge TrapCode into 2020 data
+alltemps19 <- readr::read_csv("./data/trapview_temps_degf_y19.csv")
+alltemps19
+# # A tibble: 18,969 x 6
+#   site      Date_time           degf_avg degf_lo degf_hi rh_avg
+#   <chr>     <dttm>                 <dbl>   <dbl>   <dbl>  <dbl>
+# 1 UCKearney 2019-05-16 17:00:00     56.1    55.4    56.8   83.0
+# 2 UCKearney 2019-05-16 18:00:00     57.5    56.1    58.6   81.7
+# 3 UCKearney 2019-05-16 19:00:00     55.0    53.8    56.8   90.4
+
+### Get uniform site names
 
 unique(alltemps19$site)
 # [1] "UCKearney"   "usda"        "Perez"       "MWoolf_west" "MWoolf_east"
 
+trapcodes <- read.csv("./data/trapview_trap_codes.csv")
+trapcodes
+#    Year        site TrapCode site2
+# 1  2019       Perez    tv19a     E
+# 2  2019 MWoolf_west    tv19b     A
+# 3  2019 MWoolf_east    tv19c     B
+# 4  2019        usda    tv19d     C
+# 5  2019   UCKearney    tv19e     D
+# 6  2020  mikewoolf1    tv20a    A1
+# 7  2020  mikewoolf2    tv20b    A2
+# 8  2020  mikewoolf3    tv20c    A3
+# 9  2020  mikewoolf4    tv20d    A4
+# 10 2020  mikewoolf5    tv20e    A5
+
+#   2019 names match
+
+trapcodes19 <- trapcodes[trapcodes$Year == 2019,c(2,4)]
+trapcodes19
+
 alltemps19 <- left_join(trapcodes19,alltemps19) # parallel with line 44
 head(alltemps19,2)
-#    site TrapCode           Date_time degf_avg degf_lo degf_hi rh_avg
-# 1 Perez    tv19a 2019-06-04 16:00:00     88.7    86.2    90.5  59.84
-# 2 Perez    tv19a 2019-06-04 17:00:00     84.0    82.4    85.6  69.78
+#    site site2           Date_time degf_avg degf_lo degf_hi rh_avg
+# 1 Perez     E 2019-06-04 16:00:00     88.7    86.2    90.5  59.84
+# 2 Perez     E 2019-06-04 17:00:00     84.0    82.4    85.6  69.78
 
 
+### Obtain and tweak 2020 temperatures
+alltemps20 <- readr::read_csv("./data/trapview_temps_degf_y20.csv")
 unique(alltemps20$site) #17,580 obs
 # [1] "MWT1" "MWT2" "MWT3" "MWT4" "MWT5"
 
+#  Change to names found in the trapcodes table
 alltemps20$site[alltemps20$site == "MWT1"] <- "mikewoolf1"
 alltemps20$site[alltemps20$site == "MWT2"] <- "mikewoolf2"
 alltemps20$site[alltemps20$site == "MWT3"] <- "mikewoolf3"
 alltemps20$site[alltemps20$site == "MWT4"] <- "mikewoolf4"
 alltemps20$site[alltemps20$site == "MWT5"] <- "mikewoolf5"
 
+trapcodes20 <- trapcodes[trapcodes$Year == 2020,c(2,4)]
+trapcodes20
+#          site site2
+# 6  mikewoolf1    A1
+# 7  mikewoolf2    A2
+# 8  mikewoolf3    A3
+# 9  mikewoolf4    A4
+# 10 mikewoolf5    A5
+
 alltemps20 <- left_join(trapcodes20,alltemps20) # parallel with line 44
-head(alltemps20,2)
-#         site TrapCode           Date_time degf_avg degf_lo degf_hi rh_avg
-# 1 mikewoolf1    tv20a 2020-04-22 02:00:00    51.50    50.2    52.5  90.27
-# 2 mikewoolf1    tv20a 2020-04-22 03:00:00    49.53    48.9    49.8  93.83
-
-# Modify for merge compatibility (now not used)
-# alltemps19 <- alltemps19 %>% 
-#   dplyr::rename(datetime = Date_time)
-# alltemps20 <- alltemps20 %>% 
-#   dplyr::rename(datetime = Date_time)
-
-### Make variables in alltemps19 and alltemps20 compatible with each
-### other and with the counts data set
-
-unique(counts_y19y20$TrapCode)
-# [1] "tv19a" "tv19b" "tv19c" "tv19d" "tv19e" "tv20a" "tv20b" "tv20c" "tv20d" "tv20e" 
+head(alltemps20,4)
+#   site site2           Date_time degf_avg degf_lo degf_hi rh_avg
+# 1 mikewoolf1    A1 2020-04-22 02:00:00    51.50    50.2    52.5  90.27
+# 2 mikewoolf1    A1 2020-04-22 03:00:00    49.53    48.9    49.8  93.83
+# 3 mikewoolf1    A1 2020-04-22 04:00:00    49.68    48.9    50.4  94.21
+# 4 mikewoolf1    A1 2020-04-22 05:00:00    49.52    49.1    49.8  94.05
 
 # combine the temperature data sets
 temps <- rbind(alltemps19,alltemps20)
@@ -70,11 +99,16 @@ temps
 # 1 UCKearney 2019-05-16 17:00:00     56.1    55.4    56.8   83.0
 # 2 UCKearney 2019-05-16 18:00:00     57.5    56.1    58.6   81.7
 
+#---------------------------------------------------------------------------#
+#-- 2. Add useful descriptors and save 2-year temperature data set ---------
+#---------------------------------------------------------------------------#
+
+### Set time zone
 tzone <- "America/Los_Angeles"
 lubridate::tz(temps$Date_time) <- tzone
 attr(temps$Date_time,"tzone")
 
-#### Get new variable w degrees C
+### Get new variable w degrees C
 temps <- temps %>% 
   mutate(degc_avg = (degf_avg - 32)/1.8)
 
@@ -87,105 +121,56 @@ temps <- temps %>%
          Julian = yday(Date_time),
          Hr = hour(Date_time))
 head(temps,3)
-#      site TrapCode           Date_time degf_avg degf_lo degf_hi rh_avg
-# 1   Perez    tv19a 2019-06-04 16:00:00    88.70    86.2    90.5  59.84
-# 2   Perez    tv19a 2019-06-04 17:00:00    84.00    82.4    85.6  69.78
-# 3   Perez    tv19a 2019-06-04 18:00:00    80.67    79.0    82.2  74.41
+#    site site2           Date_time degf_avg degf_lo degf_hi rh_avg degc_avg   Yr Mnth Julian Hr
+# 1 Perez     E 2019-06-04 16:00:00    88.70    86.2    90.5  59.84 31.50000 2019  Jun    155 16
+# 2 Perez     E 2019-06-04 17:00:00    84.00    82.4    85.6  69.78 28.88889 2019  Jun    155 17
+# 3 Perez     E 2019-06-04 18:00:00    80.67    79.0    82.2  74.41 27.03889 2019  Jun    155 18
 
+### Save modified 2-year temperature data set
+write.csv(temps,"temps_all.csv", row.names = FALSE)
 
-### Temperatures plot for 2019
-temps19 <- temps %>% 
-  filter(year(Date_time) == 2019)
-str(temps19)
+#---------------------------------------------------------------------------#
+#-- 3. Merge all Count and temperature data then save                 -------
+#---------------------------------------------------------------------------#
 
-p3 <- ggplot(temps19, aes(x = Date_time, y = degc_avg)) +
-  geom_line() +
-  facet_grid(TrapCode ~ .) +
-  theme_bw() +
-  #scale_x_datetime(breaks = as.Date.POSIXct(c("2020-03-01","2020-04-01","2020-05-01","2020-06-01","2020-07-01","2020-08-01","2020-09-01","2020-10-01"))) +
-  scale_x_datetime(breaks = date_breaks("1 month")) +
-  xlab("") +
-  ylab("Temperature (degrees Celcius)") +
-  theme(axis.text.x = element_text(color = "black", size = 9, angle = 45, hjust = 1),
-        axis.text.y = element_text(color = "black", size = 9),
-        axis.title.x = element_text(color = "black", size = 9),
-        axis.title.y = element_text(color = "black", size = 9),
-        legend.title = element_text(color = "black", size = 14),
-        legend.text = element_text(color = "black", size = 14))
+#  Load 2-year counts data set
+counts_y19y20 <- read_csv("./data-intermediate/counts_all.csv")
+counts_y19y20
+# A tibble: 69,390 x 7
+#   site  site2 datetime            pest_nmbr pest_dif reviewed event
+#   <chr> <chr> <dttm>                  <dbl>    <dbl> <chr>    <chr>
+# 1 Perez E     2019-06-03 16:56:00         0        0 No       NA   
+# 2 Perez E     2019-06-03 17:56:00         0        0 No       NA   
+# 3 Perez E     2019-06-03 18:56:00         0        0 No       NA   
 
-p3
+counts_y19y20 <- counts_y19y20 %>% 
+  #  Create Mnth Julian and Hr variables
+  mutate(Yr = year(datetime),
+         Mnth = month(datetime, label = TRUE, abbr = TRUE),
+         Julian = yday(datetime),
+         Hr = hour(datetime)) %>% 
+  # Pool by site2, Yr,Mnth,Julian,Hr
+  group_by(site2,Yr,Mnth,Julian,Hr) %>% 
+  summarise(count = sum(pest_dif, na.rm = TRUE))
 
-# ggsave(filename = "Y19_trapview_temps_daily_by_site.jpg", p3, path = "./results",
-#        width = 2.83, height = 5.83, dpi = 300, units = "in", device='jpg')
+counts_y19y20
+# A tibble: 36,055 x 6
+# Groups:   site2, Yr, Mnth, Julian [1,581]
+#   site2    Yr Mnth  Julian    Hr count
+#   <chr> <dbl> <ord>  <dbl> <int> <dbl>
+# 1 A      2019 May      142    15     0
+# 2 A      2019 May      142    16     0
 
+temps <- temps %>% 
+  select(site2,Yr,Mnth,Julian,Hr,degc_avg)
 
-# Temperatures plot for 2020
-temps20 <- temps %>% 
-  filter(year(Date_time) == 2020)
+head(temps,3)
+#   site2   Yr Mnth Julian Hr degc_avg
+# 1     E 2019  Jun    155 16 31.50000
+# 2     E 2019  Jun    155 17 28.88889
+# 3     E 2019  Jun    155 18 27.03889
 
-# Rename variable for prettier graph
-temps20$site[temps20$site == "mikewoolf1"] <- "West 1"
-temps20$site[temps20$site == "mikewoolf2"] <- "West 2"
-temps20$site[temps20$site == "mikewoolf3"] <- "West 3"
-temps20$site[temps20$site == "mikewoolf4"] <- "West 4"
-temps20$site[temps20$site == "mikewoolf5"] <- "West 5"
-
-
-p4 <- ggplot(temps20, aes(x = Date_time, y = degc_avg)) +
-  geom_line() +
-  facet_grid(TrapCode ~ .) +
-  theme_bw() +
-  scale_x_datetime(breaks = date_breaks("1 month")) +
-  xlab("") +
-  ylab("Temperature (degrees Celcius)") +
-  theme(axis.text.x = element_text(color = "black", size = 9, angle = 45, hjust = 1),
-        axis.text.y = element_text(color = "black", size = 9),
-        axis.title.x = element_text(color = "black", size = 9),
-        axis.title.y = element_text(color = "black", size = 9),
-        legend.title = element_text(color = "black", size = 14),
-        legend.text = element_text(color = "black", size = 14))
-
-p4
-
-# ggsave(filename = "Y20_trapview_temps_daily_by_site.jpg", p4, path = "./results",
-#        width = 2.83, height = 5.83, dpi = 300, units = "in", device='jpg')
-
-### Output combined 2019 plots
-
-p1p3 <- ggarrange(p1,p3, ncol = 2)
-p1p3
-
-ggsave(filename = "Fig2.jpg", p1p3, path = "./results",
-       width = 5.83, height = 5.83, dpi = 300, units = "in", device='jpg')
-
-### Output combined 2020 plots
-
-p2p4 <- ggarrange(p2,p4, ncol = 2)
-p2p4
-
-ggsave(filename = "Fig3.jpg", p2p4, path = "./results",
-       width = 5.83, height = 5.83, dpi = 300, units = "in", device='jpg')
-
-
-# Merge temps to counts w left join, so that records in temps are retained only
-# if they match a record in counts
-length(temps$degf_avg[is.na(temps$degf_avg)])
-# [1] 1
-#temps <- 
-temps <- temps[!is.na(temps$Date_time),] # drop the 1 NA in case that is a problem
-
-head(temps,2)
-
-#-- 5. Merge non-zero Count data to corresponding temperature records -------
-
-counts_y19y20$datetime <- NULL
-temps$datetime <- NULL
-### For counts, the hour function makes the reading more approximate. For 
-### temps, data were sent every hour on the hour and were a summary of the 
-### previous hour, so the hour function gives us something that matches the
-### counts data
-
-combined <- left_join(counts_y19y20, temps, by = c("Yr","site","Julian","Hr"))
+combined <- left_join(counts_y19y20, temps, by = c("Yr","site2","Julian","Hr"))
 head(combined)
 #   pest_dif      site Yr.x Mnth.x Julian Hr degf_avg degf_lo degf_hi rh_avg Yr.y Mnth.y
 # 1        7 UCKearney 2019    Apr    116 13       NA      NA      NA     NA   NA   <NA>
@@ -195,52 +180,5 @@ head(combined)
 # 5        3 UCKearney 2019    May    136  9       NA      NA      NA     NA   NA   <NA>
 # 6        1 UCKearney 2019    Jun    153  5    60.17    59.7    61.2  89.83 2019    Jun
 
-nrow(combined)
-# [1] 1479
-nrow(combined[complete.cases(combined), ])
-# [1] 1428
-### lose some records, but retain most
-
-combined[!complete.cases(combined), ]
-## offenders were in certain times and places
-
-combined <- combined[complete.cases(combined), ]
-combined
-
-write.csv(combined,"./data/merged_count_and_temp_data.csv",row.names = FALSE)
-
-combined19 <-combined %>% 
-  filter(Yr == 2019) %>% 
-  ggplot(aes(x = ))
-
-#-- 6. Explore high and low temps by month  ----------------------
- 
-Hi_lo <- temps %>%
-  group_by(Mnth,Julian) %>%
-  summarise(Hi = max(degf_hi),
-            Lo = min(degf_lo))
-
-DescTools::Desc(Hi ~ Mnth, data = Hi_lo)
-
-DescTools:Desc(Lo ~ Mnth, data = Hi_lo)
-
-
-### Boxplots of high and low temperature by month
-ggplot(Hi_lo, aes(x = Mnth, y = Hi)) +
-         geom_boxplot()
-
-ggplot(Hi_lo, aes(x = Mnth, y = Lo)) +
-  geom_boxplot()
-
-### Bloxplots of temperautre by hour, facet_wrap month
-### Crude, but demonstrates that temperature continues to increase until 
-### the last hour of daylight
-
-x <- c("Mar","Apr","May","Jun","Jul","Aug","Sep","Oct")
-
-temps2 <- temps  %>%
-  filter(Mnth %in% x)
-
-ggplot(temps2, aes(x = Hr, y = degf_avg, group = Hr)) +
-  geom_boxplot() +
-  facet_wrap(vars(Mnth))
+### Save 2 year combined count and temperature data file
+write.csv(combined,"./data-intermediate/combined_count_temp_all.csv", row.names = FALSE)
